16481610157861387e3310a4205a5d082533e052
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-01 16:07:49 +0530

    Made timer decorator to print time taken by functions.

--- timer_decorator.py
+++ timer_decorator.py
@@ -1,14 +1,24 @@
-import time
+import time  #importing time to measure time taken
+
+
+# ------------------------------- Timer Decorator ------------------------------- #
+
+# This decorator measures time taken by any function
 
 def timer(func):
     def wrapper(*args, **kwargs):
         print("Starting timer")
+        starting_time = time.time()
         result = func(*args, **kwargs)
-        print("Stopping timer")
         print(result)
+        print("Stopping timer")
+        stopping_time = time.time()
+        time_elapsed = stopping_time - starting_time
+        print(f"Time taken: {time_elapsed} seconds")
         return result
     return wrapper
 
+# ------------------------------- Decorated Function (Example) ------------------------------- #
 
 @timer
 def sleeping_fn():
@@ -18,4 +28,5 @@ def sleeping_fn():
     return f"Slept for {sleeping_duration} seconds"
 
 
+#calling the function to initiate process
 sleeping_fn()
