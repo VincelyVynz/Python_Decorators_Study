3f1454f200a5cc035259cb91976c835ccbc5bf91
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-01 18:08:00 +0530

    Add more options to exception handler decorator. Now prints - error type, error message and traceback for easier debugging

--- exception_handler_decorator_with_example.py
+++ exception_handler_decorator_with_example.py
@@ -1,4 +1,6 @@
 # ------------------------------- Exception Handler ------------------------------- #
+import traceback
+
 
 # Catches and prints any errors during function execution for easier debugging.
 
@@ -8,7 +10,10 @@ def error_catcher(func):
             result = func(*args, **kwargs)
             return result                          # Returns what the original function outputs in case of no errors
         except Exception as e:                     # Catches the error instead of crashing the program.
-            print(f"⚠ An Error Occurred! ⚠ {e}")   # Prints a warning message along with the error.
+            print(f"⚠ An Error Occurred! ⚠")       # Prints a warning message along with the error.
+            print(f"Error Type: {type(e).__name__}")
+            print(f"Error Message: {e}")
+            print(f"Traceback: {traceback.format_exc()}")
             return e                               # Returns the error message for easy debugging.
     return wrapper
 
